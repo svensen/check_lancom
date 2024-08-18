54a22c466bb4b862bfee23aad245d527a044f250
Sven Siemsen <sven@sven-siemsen.de>
2024-08-18 22:10:34 +0200

    Add SNMPv3 Support, add WAN connection check and stats

--- check_lancom.sh
+++ check_lancom.sh
@@ -6,6 +6,7 @@
 # MIT License
 #
 # Copyright (c) 2017 Dennis Michalski
+# Copyright (c) 2024 Sven Siemsen
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -27,23 +28,19 @@
 #
 # Revision history:
 # 2017-03-03  Created
-
+# 2024-08-18  Add SNMPv3 Support, add WAN connection check and stats
 
 # Commands
-CMD_BASENAME="$(which basename)"
-CMD_SNMPGET="$(which snmpget)"
 CMD_SNMPWALK="$(which snmpwalk)"
-CMD_SNMPTABLE="$(which snmptable)"
 CMD_AWK="$(which awk)"
-CMD_GREP="$(which grep)"
 CMD_BC="$(which bc)"
 CMD_EXPR="$(which expr)"
 
 # Script name
-SCRIPTNAME=`$CMD_BASENAME $0`
+SCRIPTNAME=$(basename "$0")
 
 # Version
-VERSION="1.0"
+VERSION="1.1"
 
 # Plugin return codes
 STATE_OK=0
@@ -52,56 +49,50 @@ STATE_CRITICAL=2
 STATE_UNKNOWN=3
 STATE_CONNECTED="eConnection(7)"
 
-#Memory
+# OIDs
 OID_MEMORYTOTAL="LCOS-MIB::lcsStatusHardwareInfoTotalMemoryKbytes"
 OID_MEMORYFREE="LCOS-MIB::lcsStatusHardwareInfoFreeMemoryKbytes"
-
-#CPU
-OID_CPULOAD="LCOS-MIB::lcsStatusHardwareInfoCpuLoadPercent"
 OID_CPULOAD5S="LCOS-MIB::lcsStatusHardwareInfoCpuLoad5sPercent"
 OID_CPULOAD60S="LCOS-MIB::lcsStatusHardwareInfoCpuLoad60sPercent"
 OID_CPULOAD300S="LCOS-MIB::lcsStatusHardwareInfoCpuLoad300sPercent"
-
-#Temperature
 OID_TEMP="LCOS-MIB::lcsStatusHardwareInfoTemperatureDegrees"
 OID_TEMPMAX="LCOS-MIB::lcsSetupTemperatureMonitorUpperLimitDegrees"
 OID_TEMPMIN="LCOS-MIB::lcsSetupTemperatureMonitorLowerLimitDegrees"
+OID_VPN_CONNECTIONS="LCOS-MIB::lcsStatusVpnTunnel"
 
-#VPN Connections
-OID_VPN_CONNECTIONS="LCOS-MIB::lcsStatusVpnConnectionsEntryState"
 
-# WAN Througput
-OID_WAN_IF_THROUGHPUT="LCOS-MIB::lcsStatusWanThroughputTable"
 
 # Default variables
 DESCRIPTION="Unknown"
 STATE=$STATE_UNKNOWN
-
-# Default options
+SNMP_VERSION=1
 COMMUNITY="public"
 HOSTNAME="192.168.10.1"
 TASK=""
 WARNING=60
 CRITICAL=65
-INTERFACE=eVdsl1
+INTERFACE=eDslCh1
+CONNECTION_NAME="INTERNET"
 
-# Option processing
+
+# SNMPv3 variables
+SEC_LEVEL=""
+SEC_NAME=""
+AUTH_PROTOCOL=""
+AUTH_PASSWORD=""
+PRIV_PROTOCOL=""
+PRIV_PASSWORD=""
+
+# Functions
 print_usage() {
-  echo "Usage: ./check_lancom -H 192.168.0.1 -T traffic -C public -w 60 -c 65 -I eVdsl1"
-  echo "  $SCRIPTNAME -H ADDRESS"
-  echo "  $SCRIPTNAME -C STRING"
-  echo "  $SCRIPTNAME -w INTEGER"
-  echo "  $SCRIPTNAME -c INTEGER"
-  echo "  $SCRIPTNAME -I STRING"
-  echo "  $SCRIPTNAME -h"
-  echo "  $SCRIPTNAME -V"
+  echo "Usage: $SCRIPTNAME -H 192.168.0.1 -P 1|2|3 -T task -C community -w warning -c critical -I connection_name"
+  echo "       $SCRIPTNAME -H 192.168.0.1 -P 3 -l secLevel -u secName -a authProtocol -A authPassword -x privProtocol -X privPassword"
 }
 
 print_version() {
-  echo $SCRIPTNAME version $VERSION
-  echo ""
+  echo "$SCRIPTNAME version $VERSION"
   echo "This nagios plugin comes with ABSOLUTELY NO WARRANTY."
-  echo "You may redistribute copies of the plugin under the terms of the MIT License."}
+  echo "You may redistribute copies of the plugin under the terms of the MIT License."
 }
 
 print_help() {
@@ -110,182 +101,202 @@ print_help() {
   print_usage
   echo ""
   echo "Checks Lancom Router"
-  echo ""
-  echo "-H ADDRESS"
-  echo "   Name or IP address of host (default: 192.168.0.1)"
-  echo "-T TASK"
-  echo "   Task to check. Must be in: temperature, cpu, memory, traffic"
-  echo "-C STRING"
-  echo "   Community name for the host SNMP agent (default: public)"
-  echo "-w INTEGER"
-  echo "   Warning level for memory usage in percent (default: 60)"
-  echo "-c INTEGER"
-  echo "   Critical level for memory usage in percent (default: 65)"
-  echo "-I INTEGER"
-  echo "   Interface to monitor traffic for (default: eVdsl1)"
-  echo "-h"
-  echo "   Print this help screen"
-  echo "-V"
-  echo "   Print version and license information"
-  echo ""
-  echo ""
 }
 
-# Plugin processing
 size_convert() {
-  if [ $VALUE -ge  1073741824 ]; then
-    VALUE=`echo "scale=2 ; ( ( $VALUE / 1024 ) / 1024 ) / 1024" | $CMD_BC`
-    VALUE="$VALUE GB"
-  elif [ $VALUE -ge 1048576 ]; then
-    VALUE=`echo "scale=2 ; ( $VALUE / 1024 ) / 1024" | $CMD_BC`
-    VALUE="$VALUE MB"
-  elif [ $VALUE -ge 1024 ]; then
-    VALUE=`echo "scale=2 ; $VALUE / 1024" | $CMD_BC`
-    VALUE="$VALUE KB"
+  local value=$1
+  if [ "$value" -ge 1073741824 ]; then
+    echo "$(echo "scale=2 ; ( ( $value / 1024 ) / 1024 ) / 1024" | $CMD_BC) TiB"
+  elif [ "$value" -ge 1048576 ]; then
+    echo "$(echo "scale=2 ; ( $value / 1024 ) / 1024" | $CMD_BC) GiB"
+  elif [ "$value" -ge 1024 ]; then
+    echo "$(echo "scale=2 ; $value / 1024" | $CMD_BC) MiB"
   else
-    VALUE="$VALUE Octets"
+    echo "$value KiB"
   fi
 }
 
-while getopts H:T:C:w:c:I:hV OPT
-do
+snmp_get_value() {
+  local oid=$1
+  local cmd="$CMD_SNMPWALK -t 2 -r 2 -v $SNMP_VERSION"
+
+  if [ "$SNMP_VERSION" == "3" ]; then
+    cmd="$cmd -l $SEC_LEVEL -u $SEC_NAME -a $AUTH_PROTOCOL -A $AUTH_PASSWORD -x $PRIV_PROTOCOL -X $PRIV_PASSWORD $HOSTNAME $oid"
+  else
+    cmd="$cmd -c $COMMUNITY $HOSTNAME $oid"
+  fi
+
+  echo $(eval $cmd | $CMD_AWK '{ print $4 }')
+}
+
+while getopts H:P:T:C:w:c:I:l:u:a:A:x:X:hV OPT; do
   case $OPT in
     H) HOSTNAME="$OPTARG" ;;
-	T) TASK="$OPTARG" ;;
+    P) SNMP_VERSION="$OPTARG" ;;
+    T) TASK="$OPTARG" ;;
     C) COMMUNITY="$OPTARG" ;;
     w) WARNING=$OPTARG ;;
     c) CRITICAL=$OPTARG ;;
     I) INTERFACE=$OPTARG ;;
-    h)
-      print_help
-      exit $STATE_UNKNOWN
-      ;;
-    V)
-      print_version
-      exit $STATE_UNKNOWN
-      ;;
-   esac
+    l) SEC_LEVEL="$OPTARG" ;;
+    u) SEC_NAME="$OPTARG" ;;
+    a) AUTH_PROTOCOL="$OPTARG" ;;
+    A) AUTH_PASSWORD="$OPTARG" ;;
+    x) PRIV_PROTOCOL="$OPTARG" ;;
+    X) PRIV_PASSWORD="$OPTARG" ;;
+    h) print_help; exit $STATE_UNKNOWN ;;
+    V) print_version; exit $STATE_UNKNOWN ;;
+  esac
 done
 
+# Check for SNMPv3 mandatory parameters
+if [ "$SNMP_VERSION" == "3" ]; then
+  if [ -z "$SEC_LEVEL" ] || [ -z "$SEC_NAME" ] || [ -z "$AUTH_PROTOCOL" ] || [ -z "$AUTH_PASSWORD" ] || [ -z "$PRIV_PROTOCOL" ] || [ -z "$PRIV_PASSWORD" ]; then
+    echo "For SNMPv3, -l secLevel, -u secName, -a authProtocol, -A authPassword, -x privProtocol, and -X privPassword are mandatory."
+    exit $STATE_UNKNOWN
+  fi
+fi
+
 if [ "$TASK" == "memory" ]; then
-	# Get memory usage from router
-	TOTALMEMORY=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_MEMORYTOTAL | $CMD_AWK '{ print $4}'`
-	FREEMEMORY=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_MEMORYFREE | $CMD_AWK '{ print $4}'`
+  TOTALMEMORY=$(snmp_get_value "$OID_MEMORYTOTAL")
+  FREEMEMORY=$(snmp_get_value "$OID_MEMORYFREE")
 
-	# Process data
-	if [ -n "$TOTALMEMORY" ] && [ -n "$FREEMEMORY" ]; then
-	  # calculate usage
-	  USEDMEMORY=`$CMD_EXPR \( $TOTALMEMORY - $FREEMEMORY \)`
-	  USEDMEMORY_POURCENT=`$CMD_EXPR \( $USEDMEMORY \* 100 \) / $TOTALMEMORY`
+  if [ -n "$TOTALMEMORY" ] && [ -n "$FREEMEMORY" ]; then
+    USEDMEMORY=$((TOTALMEMORY - FREEMEMORY))
+    USEDMEMORY_POURCENT=$((USEDMEMORY * 100 / TOTALMEMORY))
 
-	  if [ $WARNING != 0 ] || [ $CRITICAL != 0 ]; then
-		PERFDATA_WARNING=`$CMD_EXPR \( $TOTALMEMORY \* $WARNING \) / 100`
-		PERFDATA_CRITICAL=`$CMD_EXPR \( $TOTALMEMORY \* $CRITICAL \) / 100`
-		if [ $USEDMEMORY_POURCENT -gt $CRITICAL ] && [ $CRITICAL != 0 ]; then
-		  STATE=$STATE_CRITICAL
-		elif [ $USEDMEMORY_POURCENT -gt $WARNING ] && [ $WARNING != 0 ]; then
-		  STATE=$STATE_WARNING
-		else
-		  STATE=$STATE_OK
-		fi
+    if [ "$USEDMEMORY_POURCENT" -gt "$CRITICAL" ]; then
+      STATE=$STATE_CRITICAL
+    elif [ "$USEDMEMORY_POURCENT" -gt "$WARNING" ]; then
+      STATE=$STATE_WARNING
+    else
+      STATE=$STATE_OK
+    fi
+    # Results are in Kilobytes, make it human-readable
+    USEDMEMORY_FORMAT=$(size_convert "$USEDMEMORY")
+    FREEMEMORY_FORMAT=$(size_convert "$FREEMEMORY")
+    TOTALMEMORY_FORMAT=$(size_convert "$TOTALMEMORY")
 
-		VALUE=$USEDMEMORY
-		size_convert
-		USEDMEMORY_FORMAT=$VALUE
+    DESCRIPTION="Memory usage : $USEDMEMORY_FORMAT used for a total of $TOTALMEMORY_FORMAT (${USEDMEMORY_POURCENT}%)"
+    DESCRIPTION="${DESCRIPTION}| used=${USEDMEMORY}KB;${WARNING}%;${CRITICAL}%;0"
 
-		VALUE=$FREEMEMORY
-		size_convert
-		FREEMEMORY_FORMAT=$VALUE
-
-		VALUE=$TOTALMEMORY
-		size_convert
-		TOTALMEMORY_FORMAT=$VALUE
-
-		DESCRIPTION="Memory usage : $USEDMEMORY_FORMAT used for a total of $TOTALMEMORY_FORMAT (${USEDMEMORY_POURCENT}%)"
-		DESCRIPTION="${DESCRIPTION}| used=${USEDMEMORY}B;$PERFDATA_WARNING;$PERFDATA_CRITICAL;0"
-
-	  else
-		echo "Values not allowed"
-		exit $STATE_UNKNOWN
-	  fi
-	else
-	  echo "Values may not be NULL"
-	  exit $STATE_UNKNOWN
-	fi
+  else
+    echo "Values may not be NULL"
+    exit $STATE_UNKNOWN
+  fi
 fi
 
 if [ "$TASK" == "cpu" ]; then
-	# Get CPU Load in Percent
-	CPULOAD5S=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_CPULOAD5S | $CMD_AWK '{ print $4 }'`
-	CPULOAD60S=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_CPULOAD60S | $CMD_AWK '{ print $4 }'`
-	CPULOAD300S=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_CPULOAD300S | $CMD_AWK '{ print $4 }'`
-	CPULOAD=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_CPULOAD | $CMD_AWK '{ print $4 }'`
+  CPULOAD5S=$(snmp_get_value "$OID_CPULOAD5S")
+  CPULOAD60S=$(snmp_get_value "$OID_CPULOAD60S")
+  CPULOAD300S=$(snmp_get_value "$OID_CPULOAD300S")
 
-	if [ $WARNING != 0 ] || [ $CRITICAL != 0 ]; then
-	  if [ $CPULOAD5S -gt $CRITICAL ] && [ $CRITICAL != 0 ]; then
-		STATE=$STATE_CRITICAL
-	  elif [ $CPULOAD60S -gt $CRITICAL ] && [ $CRITICAL != 0 ]; then
-		STATE=$STATE_CRITICAL
-	  elif [ $CPULOAD300S -gt $CRITICAL ] && [ $CRITICAL != 0 ]; then
-		STATE=$STATE_CRITICAL
-	  elif [ $CPULOAD5S -gt $WARNING ] && [ $WARNING != 0 ]; then
-		STATE=$STATE_WARNING
-	  elif [ $CPULOAD60S -gt $WARNING ] && [ $WARNING != 0 ]; then
-		STATE=$STATE_WARNING
-	  elif [ $CPULOAD300S -gt $WARNING ] && [ $WARNING != 0 ]; then
-		STATE=$STATE_WARNING
-	  else
-		STATE=$STATE_OK
-	  fi
-	fi
+  if [ "$CPULOAD5S" -gt "$CRITICAL" ] || [ "$CPULOAD60S" -gt "$CRITICAL" ] || [ "$CPULOAD300S" -gt "$CRITICAL" ]; then
+    STATE=$STATE_CRITICAL
+  elif [ "$CPULOAD5S" -gt "$WARNING" ] || [ "$CPULOAD60S" -gt "$WARNING" ] || [ "$CPULOAD300S" -gt "$WARNING" ]; then
+    STATE=$STATE_WARNING
+  else
+    STATE=$STATE_OK
+  fi
 
-	DESCRIPTION="CPU Load : $CPULOAD5S%, $CPULOAD60S%, $CPULOAD300S% | cpu_load_current=$CPULOAD;$WARNING;$CRITICAL;0 cpu_load_average5S=$CPULOAD5S;$WARNING;$CRITICAL;0 cpu_load_average60S=$CPULOAD60S;$WARNING;$CRITICAL;0 cpu_load_average300S=$CPULOAD300S;$WARNING;$CRITICAL;0"
+  DESCRIPTION="CPU Load : $CPULOAD5S%, $CPULOAD60S%, $CPULOAD300S% | cpu_load_current=$CPULOAD5S;$WARNING;$CRITICAL;0 cpu_load_average60S=$CPULOAD60S;$WARNING;$CRITICAL;0 cpu_load_average300S=$CPULOAD300S;$WARNING;$CRITICAL;0"
 fi
 
 if [ "$TASK" == "temperature" ]; then
-	# Get Temperature in degrees Celsius
-	TEMP=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_TEMP | $CMD_AWK '{ print $4 }'`
-	TEMPMIN=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_TEMPMIN | $CMD_AWK '{ print $4 }'`
-	TEMPMAX=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_TEMPMAX | $CMD_AWK '{ print $4 }'`
+  TEMP=$(snmp_get_value "$OID_TEMP")
+  TEMPMIN=$(snmp_get_value "$OID_TEMPMIN")
+  TEMPMAX=$(snmp_get_value "$OID_TEMPMAX")
 
-	if [ $WARNING -gt $TEMPMAX ] || [ $CRITICAL -gt $TEMPMAX ]; then
-	  echo "Value not allowed"
-	  exit $STATE_UNKNOWN
-	fi
+  if [ "$TEMP" -gt "$CRITICAL" ]; then
+    STATE=$STATE_CRITICAL
+  elif [ "$TEMP" -gt "$WARNING" ]; then
+    STATE=$STATE_WARNING
+  else
+    STATE=$STATE_OK
+  fi
 
-	if [ $WARNING != 0 ] || [ $CRITICAL != 0 ]; then
-	  if [ $TEMP -gt $WARNING ] && [ $WARNING != 0 ]; then
-		STATE=$STATE_WARNING
-	  elif [ $TEMP -gt $CRITICAL ] && [ $CRITICAL != 0 ]; then
-		STATE=$STATE_CRITICAL
-	  else
-		STATE=$STATE_OK
-	  fi
-	fi
-	DESCRIPTION="Temperature : $TEMP°C | temperature=$TEMP;$WARNING;$CRITICAL;0"
+  DESCRIPTION="Temperature : $TEMP°C | temperature=$TEMP;$WARNING;$CRITICAL;0"
 fi
 
 if [ "$TASK" == "vpn" ]; then
-        ERROR_CONNS=""
-        STATE=$STATE_UNKNOWN
-        CONNS=`$CMD_SNMPWALK -t 2 -r 2 -v 1 -c $COMMUNITY $HOSTNAME $OID_VPN_CONNECTIONS`
-        while read -r line; do
-            VALUE=`$CMD_AWK -F" " '{ print $4 }' <<< "$line"`
-            CONN_NAME=`$CMD_AWK -F" " '{ print $1 }' <<< "$line"`
-            CONN_NAME=`$CMD_AWK -F" " '{ split($0,a,"."); print a[2]; }' <<< "$CONN_NAME"`
-            if [ $VALUE != $STATE_CONNECTED ]; then
-                ERROR_CONNS="$ERROR_CONNS $CONN_NAME"
-                STATE=$STATE_CRITICAL
-                DESCRIPTION="VPN Connections : Critical. The following conenctions are not in State Connected: $ERROR_CONNS"
-            fi
-        done <<< "$CONNS"
-        if [ $STATE != $STATE_CRITICAL ]; then
-            STATE=$STATE_OK
-            DESCRIPTION="VPN Connections : OK"
-        fi
-        #echo $CONNS
+  STATE=$STATE_UNKNOWN
+  CONNS=$(snmp_get_value "$OID_VPN_CONNECTIONS")
+  STATE=$STATE_OK
+  DESCRIPTION="VPN Connections : OK | vpn_connections=$CONNS;$WARNING;$CRITICAL;0"
 fi
 
+check_wan_connection() {
+  local connection_name="$1"
 
-echo $DESCRIPTION
+  # Base SNMP command with version check
+  local snmp_cmd="$CMD_SNMPWALK -t 2 -r 2 -v $SNMP_VERSION"
+
+  # Add SNMPv3 specific parameters if version 3 is selected
+  if [ "$SNMP_VERSION" == "3" ]; then
+    snmp_cmd="$snmp_cmd -l $SEC_LEVEL -u $SEC_NAME -a $AUTH_PROTOCOL -A $AUTH_PASSWORD -x $PRIV_PROTOCOL -X $PRIV_PASSWORD"
+  else
+    snmp_cmd="$snmp_cmd -c $COMMUNITY"
+  fi
+
+  # Perform SNMP walk to get the connection entries and statuses using the LCOS-MIB
+  local conn_entries
+  conn_entries=$($snmp_cmd $HOSTNAME -m LCOS-MIB "1.3.6.1.4.1.2356.11.1.14")
+
+  # Determine the channel associated with the specified connection name
+  local channel_oid=""
+  local channel_id=""
+  while read -r line; do
+    if echo "$line" | grep -q "$connection_name"; then
+      channel_oid=$(echo "$line" | $CMD_AWK '{print $1}')
+      channel_id=$(echo "$channel_oid" | awk -F'.' '{print $NF}')
+      break
+    fi
+  done <<< "$conn_entries"
+
+  # Check if channel_id was found
+  if [ -z "$channel_id" ]; then
+    echo "WAN Connection: UNKNOWN - Connection name '$connection_name' not found"
+    exit $STATE_UNKNOWN
+  fi
+
+  # OID to check connection status
+  local conn_status_oid="LCOS-MIB::lcsStatusInfoConnectionEntryStatus.$channel_id"
+  local conn_status
+  conn_status=$($snmp_cmd $HOSTNAME "$conn_status_oid" | $CMD_AWK '{ print $4 }')
+
+  if [ "$conn_status" != "Connection" ]; then
+    echo "WAN Connection: CRITICAL - '$connection_name' is not connected"
+    exit $STATE_CRITICAL
+  fi
+
+  # Get RX/TX per second and average for the identified channel
+  local rx_current_oid="LCOS-MIB::lcsStatusWanThroughputEntryRxSCurrent.$channel_id"
+  local tx_current_oid="LCOS-MIB::lcsStatusWanThroughputEntryTxSCurrent.$channel_id"
+  local rx_avg_oid="LCOS-MIB::lcsStatusWanThroughputEntryRxSAverage.$channel_id"
+  local tx_avg_oid="LCOS-MIB::lcsStatusWanThroughputEntryTxSAverage.$channel_id"
+
+  local rx_current
+  local tx_current
+  local rx_avg
+  local tx_avg
+
+  rx_current=$($snmp_cmd $HOSTNAME "$rx_current_oid" | $CMD_AWK '{ print $4 }')
+  tx_current=$($snmp_cmd $HOSTNAME "$tx_current_oid" | $CMD_AWK '{ print $4 }')
+  rx_avg=$($snmp_cmd $HOSTNAME "$rx_avg_oid" | $CMD_AWK '{ print $4 }')
+  tx_avg=$($snmp_cmd $HOSTNAME "$tx_avg_oid" | $CMD_AWK '{ print $4 }')
+
+  # Construct the output with metrics
+  echo -n "WAN Connection: OK - '$connection_name' is connected"
+  echo "| rx_current=${rx_current}bps tx_current=${tx_current}bps rx_avg=${rx_avg}bps tx_avg=${tx_avg}bps"
+
+  exit $STATE_OK
+}
+
+# Adding the check for the wan task
+if [ "$TASK" == "wan" ]; then
+  check_wan_connection "$INTERFACE"
+fi
+
+echo "$DESCRIPTION"
 exit $STATE
+
